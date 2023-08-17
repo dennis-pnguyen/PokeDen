4d906fab95ffa178e4de5ffbc90a62ae871772b6
Dennis Nguyen <dennis.nguyen9925@gmail.com>
2023-08-17 19:30:16 +0000

    Updated GET request #2 for getting individual pokemon data to render on entries list.

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
-const $pokemonEntryList = document.querySelector('#pokedex-entries');
+const $pokedexList = document.querySelector('#pokedex-entries');
+const $pokemonEntry = document.querySelector('.pokemon-entry');
 
 function getPokemonImage() {
   const xhr = new XMLHttpRequest();
-  xhr.open('GET', 'https://pokeapi.co/api/v2/pokemon?limit=151');
+  xhr.open('GET', 'https://pokeapi.co/api/v2/pokemon?limit=20');
   xhr.responseType = 'json';
   xhr.addEventListener('load', function () {
     for (let i = 0; i < xhr.response.results.length; i++) {
@@ -27,13 +28,56 @@ function getPokemonImage() {
       const $img = document.createElement('img');
       $img.setAttribute('class', 'pokemon-img');
       $img.src = url;
-      $pokemonEntryList.appendChild($pokeWrap);
+      $pokedexList.appendChild($pokeWrap);
       $pokeWrap.appendChild($pokeCard);
       $pokeCard.appendChild($pokeNum);
       $pokeCard.appendChild($img);
       $pokeCard.appendChild($pokeName);
+
+      $pokeWrap.addEventListener('click', handleClick);
     }
   });
   xhr.send();
 }
 getPokemonImage();
+
+function handleClick(e) {
+  $pokedexList.classList.add('hidden');
+  $pokemonEntry.classList.remove('hidden');
+  const pokemonNumber = e.target.closest('p').innerText;
+  let hash = pokemonNumber.split('#');
+  hash = hash[1].trim();
+
+  getPokemonData(hash);
+}
+
+function getPokemonData(hash) {
+  const xhrData = new XMLHttpRequest();
+  xhrData.open('GET', `https://pokeapi.co/api/v2/pokemon/${hash}`);
+  xhrData.responseType = 'json';
+  xhrData.addEventListener('load', function () {
+    const pokeData = xhrData.response;
+    const $entryDiv = document.createElement('div');
+    $entryDiv.setAttribute('class', 'card-wrapper');
+    const $entryCard = document.createElement('div');
+    $entryCard.setAttribute('class', 'pokemon-card');
+    const $entryNum = document.createElement('p');
+    $entryNum.setAttribute('class', 'pokemon-number');
+    $entryNum.textContent = '#' + pokeData.id;
+    const $entryImg = document.createElement('img');
+    $entryImg.setAttribute('class', 'pokemon-img');
+    $entryImg.setAttribute('alt', pokeData.name);
+    $entryImg.src = pokeData.sprites.other.dream_world.front_default;
+    const $entryName = document.createElement('h3');
+    $entryName.setAttribute('class', 'pokemon-name');
+    $entryName.textContent = pokeData.name;
+
+    $pokemonEntry.appendChild($entryDiv);
+    $entryDiv.appendChild($entryCard);
+    $entryCard.appendChild($entryNum);
+    $entryCard.appendChild($entryImg);
+    $entryCard.appendChild($entryName);
+
+  });
+  xhrData.send();
+}
