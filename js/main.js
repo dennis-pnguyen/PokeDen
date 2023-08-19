fe73c2fc73ab8d131025577e3794c8c58f01e395
Dennis Nguyen <dennis.nguyen9925@gmail.com>
2023-08-19 21:10:05 +0000

    Refactored functions in order to reduce load function in GET requests.

--- js/main.js
+++ js/main.js
@@ -8,39 +8,45 @@ function getPokemonImage() {
   xhr.responseType = 'json';
   xhr.addEventListener('load', function () {
     for (let i = 0; i < xhr.response.results.length; i++) {
-      const currentPokemon = xhr.response.results[i];
-      let url;
-      if (i + 1 < 10) {
-        url = `https://raw.githubusercontent.com/HybridShivam/Pokemon/master/assets/images/00${i + 1}.png`;
-      } else if (i + 1 < 100) {
-        url = `https://raw.githubusercontent.com/HybridShivam/Pokemon/master/assets/images/0${i + 1}.png`;
-      } else {
-        url = `https://raw.githubusercontent.com/HybridShivam/Pokemon/master/assets/images/${i + 1}.png`;
-      }
-      const $pokeWrap = document.createElement('div');
-      $pokeWrap.setAttribute('class', 'card-wrapper');
-      const $pokeCard = document.createElement('div');
-      $pokeCard.setAttribute('class', 'pokemon-card');
-      const $pokeNum = document.createElement('p');
-      $pokeNum.setAttribute('class', 'pokemon-number');
-      $pokeNum.textContent = `#${i + 1}`;
-      const $pokeName = document.createElement('h2');
-      $pokeName.textContent = currentPokemon.name;
-      const $img = document.createElement('img');
-      $img.setAttribute('class', 'pokemon-img');
-      $img.src = url;
-      $pokedexList.appendChild($pokeWrap);
-      $pokeWrap.appendChild($pokeCard);
-      $pokeCard.appendChild($pokeNum);
-      $pokeCard.appendChild($img);
-      $pokeCard.appendChild($pokeName);
-
-      $pokeWrap.addEventListener('click', handleClick);
+      $pokedexList.appendChild(renderPokemon(xhr.response.results[i], i));
     }
   });
   xhr.send();
 }
 getPokemonImage();
+
+function renderPokemon(pokemon, index) {
+  const currentPokemon = pokemon;
+  let url;
+  if (index + 1 < 10) {
+    url = `https://raw.githubusercontent.com/HybridShivam/Pokemon/master/assets/images/00${index + 1}.png`;
+  } else if (index + 1 < 100) {
+    url = `https://raw.githubusercontent.com/HybridShivam/Pokemon/master/assets/images/0${index + 1}.png`;
+  } else {
+    url = `https://raw.githubusercontent.com/HybridShivam/Pokemon/master/assets/images/${index + 1}.png`;
+  }
+  const $pokeWrap = document.createElement('div');
+  $pokeWrap.setAttribute('class', 'card-wrapper');
+  const $pokeCard = document.createElement('div');
+  $pokeCard.setAttribute('class', 'pokemon-card');
+  const $pokeNum = document.createElement('p');
+  $pokeNum.setAttribute('class', 'pokemon-number');
+  $pokeNum.textContent = `#${index + 1}`;
+  const $pokeName = document.createElement('h2');
+  $pokeName.textContent = currentPokemon.name;
+  const $img = document.createElement('img');
+  $img.setAttribute('class', 'pokemon-img');
+  $img.setAttribute('alt', currentPokemon.name);
+  $img.src = url;
+  $pokeWrap.appendChild($pokeCard);
+  $pokeCard.appendChild($pokeNum);
+  $pokeCard.appendChild($img);
+  $pokeCard.appendChild($pokeName);
+
+  $pokeWrap.addEventListener('click', handleClick);
+
+  return $pokeWrap;
+}
 
 function handleClick(e) {
   $pokedexList.classList.add('hidden');
@@ -57,42 +63,7 @@ function getPokemonData(hash) {
   xhrData.open('GET', `https://pokeapi.co/api/v2/pokemon/${hash}`);
   xhrData.responseType = 'json';
   xhrData.addEventListener('load', function () {
-    const pokeData = xhrData.response;
-    const $entryDiv = document.createElement('div');
-    $entryDiv.setAttribute('class', 'card-wrapper');
-    const $entryCard = document.createElement('div');
-    $entryCard.setAttribute('class', 'pokemon-card');
-    const $entryNum = document.createElement('p');
-    $entryNum.setAttribute('class', 'pokemon-number');
-    $entryNum.textContent = '#' + pokeData.id;
-    const $entryImg = document.createElement('img');
-    $entryImg.setAttribute('class', 'pokemon-img');
-    $entryImg.setAttribute('alt', pokeData.name);
-    $entryImg.src = pokeData.sprites.other.dream_world.front_default;
-    const $entryName = document.createElement('h3');
-    $entryName.setAttribute('class', 'pokemon-name');
-    $entryName.textContent = pokeData.name;
-
-    $pokemonEntry.appendChild($entryDiv);
-    $entryDiv.appendChild($entryCard);
-    $entryCard.appendChild($entryNum);
-    $entryCard.appendChild($entryImg);
-    $entryCard.appendChild($entryName);
-
-    const $textDiv = document.createElement('div');
-    $textDiv.setAttribute('class', 'type-box');
-    const $textBox = document.createElement('div');
-    $textBox.setAttribute('class', 'pokemon-typebox');
-    const $typeHeader = document.createElement('div');
-    $typeHeader.setAttribute('class', 'pokemon-type-header');
-    const $pokeType = document.createElement('p');
-    $pokeType.setAttribute('class', 'pokemon-type');
-    $pokeType.textContent = pokeData.types[0].type.name;
-
-    $typeText.appendChild($textDiv);
-    $textDiv.appendChild($textBox);
-    $textBox.appendChild($typeHeader);
-    $typeHeader.appendChild($pokeType);
+    $pokemonEntry.appendChild(renderEntry(xhrData.response));
   });
   xhrData.send();
   const xhrSpecies = new XMLHttpRequest();
@@ -114,4 +85,43 @@ function getPokemonData(hash) {
 
   });
   xhrSpecies.send();
+}
+
+function renderEntry(pokemon) {
+  const pokeData = pokemon;
+  const $entryDiv = document.createElement('div');
+  $entryDiv.setAttribute('class', 'card-wrapper');
+  const $entryCard = document.createElement('div');
+  $entryCard.setAttribute('class', 'pokemon-card');
+  const $entryNum = document.createElement('p');
+  $entryNum.setAttribute('class', 'pokemon-number');
+  $entryNum.textContent = '#' + pokeData.id;
+  const $entryImg = document.createElement('img');
+  $entryImg.setAttribute('class', 'pokemon-img');
+  $entryImg.setAttribute('alt', pokeData.name);
+  $entryImg.src = pokeData.sprites.other.dream_world.front_default;
+  const $entryName = document.createElement('h3');
+  $entryName.setAttribute('class', 'pokemon-name');
+  $entryName.textContent = pokeData.name;
+
+  $pokemonEntry.appendChild($entryDiv);
+  $entryDiv.appendChild($entryCard);
+  $entryCard.appendChild($entryNum);
+  $entryCard.appendChild($entryImg);
+  $entryCard.appendChild($entryName);
+
+  const $textDiv = document.createElement('div');
+  $textDiv.setAttribute('class', 'type-box');
+  const $textBox = document.createElement('div');
+  $textBox.setAttribute('class', 'pokemon-typebox');
+  const $typeHeader = document.createElement('div');
+  $typeHeader.setAttribute('class', 'pokemon-type-header');
+  const $pokeType = document.createElement('p');
+  $pokeType.setAttribute('class', 'pokemon-type');
+  $pokeType.textContent = pokeData.types[0].type.name;
+
+  $typeText.appendChild($textDiv);
+  $textDiv.appendChild($textBox);
+  $textBox.appendChild($typeHeader);
+  $typeHeader.appendChild($pokeType);
 }
